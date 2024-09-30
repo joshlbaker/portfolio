e58a544a8b1fc3a3122a46f8ce923ec4365997ef
Josh Baker <hellojoshbaker@gmail.com>
2024-09-30 16:51:47 -0400

    Initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,43 @@
+import Head from "next/head";
 import Image from "next/image";
+
+import GithubIcon from './icons/github.svg'
+import MailIcon from './icons/mail.svg'
+import LinkedInIcon from './icons/linkedin.svg'
+import MapPin from './icons/map-pin.svg'
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <Head>
+        <title>Josh Baker</title>
+      </Head>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          <div className="text-center">
+            <h1 className="text-2xl font-medium">Josh Baker</h1>
+            <p className="text-base italic">Software Developer / Cat Dad</p>
+            <p className="text-base"> <Image src={MapPin} className="inline" width={16} alt="Location icon" /> Pittsburgh, Pa</p>
+            <p className="text-base">Ex Shopify</p>
+
+            <div className="flex items-center justify-center mt-8">
+              <a href="https://github.com/joshlbaker" target="_blank" className="mx-2 pb-1 border-transparent hover:border-black hover:dark:border-white border-b-2">
+                <Image src={GithubIcon} className="w-6" alt="Github icon" />
+              </a>
+              <a href="https://www.linkedin.com/in/joshlbaker/" target="_blank" className="mx-2 pb-1 border-transparent hover:border-black hover:dark:border-white border-b-2">
+                <Image src={LinkedInIcon} className="w-6" alt="LinkedIn Icon" />
+              </a>
+              <a href="mailto:hellojoshbaker@gmail.com" target="_blank" className="mx-2 pb-1 border-transparent hover:border-black hover:dark:border-white border-b-2">
+                <Image src={MailIcon} className="w-6" alt="Mail icon" />
+              </a>
+            </div>
+
+            <p className="text-xl mt-8">Site under construction, check back soon!</p>
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
